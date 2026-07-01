72ff9b6b2644a0a4ee59f2af427b3c57741b0c8d
Sofia <sofiapulice13@gmail.com>
2026-07-01 03:07:40 -0300

    denunciar usuario

--- packages/api/src/domain/content-report/types/content-report-target-type.ts
+++ packages/api/src/domain/content-report/types/content-report-target-type.ts
@@ -1,16 +1,19 @@
 export const ContentReportTargetType = {
   CHAT: 'CHAT',
   POST: 'POST',
+  USER: 'USER',
 } as const
 
 export const contentReportTargetTypeMap: Record<ContentReportTargetType, number> = {
   [ContentReportTargetType.CHAT]: 1,
   [ContentReportTargetType.POST]: 2,
+  [ContentReportTargetType.USER]: 3,
 }
 
 export const contentReportTargetTypeMapReverse: Record<number, ContentReportTargetType> = {
   [1]: ContentReportTargetType.CHAT,
   [2]: ContentReportTargetType.POST,
+  [3]: ContentReportTargetType.USER,
 }
 
 export type ContentReportTargetType = typeof ContentReportTargetType[keyof typeof ContentReportTargetType]
